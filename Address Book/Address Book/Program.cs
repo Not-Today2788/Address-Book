1343a41c5e662b209201ae846d44ecdd9b1ff5d6
Prakhar <prakhar2788@gmail.com>
2024-01-29 11:40:57 +0530

    [Prakhar] Add: Adding the UC1 of AddressBook

---
+++ Address Book/Address Book/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book
+{
+    
+    class AddressBook
+
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+        }
+    }
+}
